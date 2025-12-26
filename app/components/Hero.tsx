ed7fc32f7cac02401773150dde8ebbaf5309f52c
Harsha Asapu <harshaasapu.b@gmail.com>
2025-12-26 10:53:14 -0600

    removed time on hero page

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -29,7 +29,7 @@ export default function Hero() {
             <span className="h-2 w-2 rounded-full bg-green-400 animate-pulse" />
             Open to work
           </span>
-          <span>{time}</span>
+          
         </div>
 
         {/* Hero content */}
